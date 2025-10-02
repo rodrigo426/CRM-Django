7c7042fe97468176b3b6a8d68b105c7696a26ff1
Rodrigo Moura <rodrigomoura426@hotmail.com>
2025-10-02 16:58:49 -0300

    login system implementation

--- crmapp/views.py
+++ crmapp/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render (request, 'home.html', {})
+    #Verificar login
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #Autenticação
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login realizado com sucesso!")
+            return redirect('home')
+        else:
+            messages.success(request, "Login sem sucesso, tente novamente!")
+            return redirect('home')
+    else:
+        return render (request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logout realizado.")
+    return redirect('home')
